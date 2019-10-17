efeea46d87b3213da929785c1354602991df89ff
everywherewego <964353527@qq.com>
2019-10-17 11:58:02 +0800

    修改pom 修改启动脚本

--- 01.terminal/start.sh
+++ 01.terminal/start.sh
@@ -3,4 +3,4 @@ script_abs=$(readlink -f "$0")
 path=${script_abs%/bin*}
 
 echo $path
-java -jar $path/${jarname}-0.0.1.jar 1>$path/log 2>$path/log &
+java -jar $path/${jarname}-0.0.1.jar 1>$path/log.log 2>$path/log.log &
